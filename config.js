94289956db24fb7d4a87c2839fc9c6c42ba6d1b2
minquan <91259011+kpneee@users.noreply.github.com>
2021-09-28 19:15:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Helu cậu ',
+    introDesc: `cho bố hỏi mấy câu đê nhưng mà phải trả lời thật lòng không tao đấm chết `,
+    btnIntro: 'Accept',
+    title: 'M yêu Bảo Long đúng không??',
+    desc: '<3',
+    btnYes: 'yêu lắm lắm',
+    btnNo: 'đell',
+    question:'tại sao lại thế',
+    btnReply: 'vvvv',
+    reply: 'vì bảo long đẹp trai vcl',
+    mess: 'bạn chọn chuẩn mẹ luôn r đấy',
+    messDesc: 'P/s:ấn accept có 1 bất ngờ chờ bạn',
+    btnAccept: 'Accept',
+    messLink: 'https://scontent.fhan5-6.fna.fbcdn.net/v/t1.15752-9/243176749_620902918902788_3818735263902293295_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_ohc=ZR-rW1lRL5AAX8NdhB0&tn=aWm_EuGfg-5euQJz&_nc_ht=scontent.fhan5-6.fna&oh=80fb335c95f2f0c3eacc76fa0b2307b0&oe=6179EDA6', //https://www.facebook.com/profile.php?id=100037435576667
+    bgLink: 'https://bom.to/NXqty2', //link ảnh nền
+    iputBgLink: 'https://bom.to/FBg6qV', //link iputBgLink
+    lookMeLink: 'https://bom.to/6iKS46', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
